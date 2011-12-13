4ebf8b4b1a7b2bdfd82bea046774e698be0f62d8
Tiago Peczenyj <peczenyj@corp.globo.com>
2011-12-13 10:38:24 -0200

    add comments in Pipe.pm

--- Upload-Pipe/lib/Upload/Pipe.pm
+++ Upload-Pipe/lib/Upload/Pipe.pm
@@ -9,9 +9,18 @@ use JSON;
 
 my $mt = MIME::Types->new();
 
+#
+# Proof of concept using AnyEvent::HTTPD
+# AnyEvent is a framework to do event-based programming and provides non-blocking I/O
+# AnyEvent::HTTP is a simple lightweight event based web (application) server
+#  with connection_class I can pass my own connection class.
+#  it is very important because the AnyEvent::HTTPD::HTTPConnection store and parse
+#  the entire body, and I can extend this class easily
+#
+
 my $httpd = AnyEvent::HTTPD->new (
 	port => 5000,
-#	connection_class => 'AnyEvent::HTTPD::HTTPConnection2'
+#	connection_class => 'AnyEvent::HTTPD::HTTPConnection2' -- or other name
 );
 
 $httpd->reg_cb (
