08ddc8eeef0403303b9e28c6df4d80c0b06f90c6
Tiago Peczenyj <peczenyj@corp.globo.com>
2011-12-24 11:00:23 -0200

    fix scala, try again 2

--- scala/project/build/WebAppBuild.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-class WebAppBuild(info: ProjectInfo) extends DefaultWebProject(info) {
-val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test" 
-val servletApi = "javax.servlet" % "servlet-api" % "2.5"
-}
